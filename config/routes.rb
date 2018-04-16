340cff50e05571ddc9e7ae1a456cafbe85145b60
David C. Ledford <dcledford@ymail.com>
2018-04-15 23:49:34 -0400

    added switch from memories to java and made java the root

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get 'javascript', to: "my_java#index"
+  get 'javascript/hello_world', to: "my_java#hello_world"
 
   resources :memories
 
-  root 'memories#index'
+  root 'my_java#index'
 end
